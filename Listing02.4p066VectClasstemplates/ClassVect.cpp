d15f2e3e3074187e7fa3cf803de389c279d53353
moueza <>
2022-02-07 01:34:42 +0100

    sov

---
+++ Listing02.4p066VectClasstemplates/ClassVect.cpp
@@ -0,0 +1,20 @@
+// p060
+// parallel-visualization-of-scientific-data-using-blender
+#include <iostream>
+#include <cmath>
+#include <cassert>
+using namespace std;
+ 
+template <class T>
+class Vect
+{
+public:
+  
+};
+ 
+
+// void main()
+int main()
+{
+  
+}
