7b4a1860f6a396c0c5019acb1123ce602eb905c0
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2024-03-13 04:01:44 +0100

    Feat: p66

--- Listing02.4p066VectClasstemplates/ClassVect.cpp
+++ Listing02.4p066VectClasstemplates/ClassVect.cpp
@@ -1,20 +1,78 @@
 // p060
 // parallel-visualization-of-scientific-data-using-blender
+
 #include <iostream>
-#include <cmath>
 #include <cassert>
 using namespace std;
- 
-template <class T>
+
+
+template <class_T>
 class Vect
-{
-public:
-  
+{public:
+  int n;
+  T *v;
+  void Affiche (char* Message) const;
 };
+
+Vect (int nn): n(nn), v(new T[n]) {assert (y || !n);}
+Vect(const Vect & w);
+   
+~Vect() {if (y) delete [] v;}
+void      operator = (const Vect & a);
+T & operator[] (int i) {assert (v && i>=0 && i <n);return v[i];}
+};
+
+template <class T>
+Vect<T>::Vect (const Vect &w) :n (w.n), v(new T[n])
+{  (*this)=w; }
+ 
+template<class T> 
+void Vect<T>::operator= (const Vect & a)
+{ assert(!n || (n==a.n &&v&& a.v);
+       	 for (int i=0;i<n;i++)
  
 
-// void main()
-int main()
-{
+
+
+
+
+
+
+
+
   
-}
+
+
+
+	   v[i]=a.v[i];
+	 template <class T>
+	 void Vect<T>::Affiche (char* Message) const
+	 {
+	   cout<<Message<<endl;
+	   for (int i=0;i<n;i++)
+	     cout<<"Element i="<<i<<"\t"<<v[i]<<endl;
+
+	 }
+	 template<class U>
+	 void InitVect(Vect&U>) & v)
+    for (int i=0;i<v.n; i++)
+      v[i]= (U)/v.n;
+  typedef Vect<double> VectR;
+
+
+       
+  void main()
+  {
+    Vect<double> w (3);
+
+    VectR
+      u (3);
+    InitVect (w); w.Affiche ("--- vecteur w ---");
+    u=w; u[0]=10;
+    u. Affiche ("--- vecteur u ---");
+    u.Affiche ("---
+vecteur u ---");
+Vect<int>
+s(3);
+InitVect(s);s.Affiche ("--vecteur s ---");
+	   }
