46cddac1a4d00f171b33c13df118cefdce69e240
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2024-03-19 07:52:17 +0100

    Chore: before Autotools

---
+++ Listing02.5p70HetitPolymorphWIP/ClassAB.cpp
@@ -0,0 +1,46 @@
+// p070
+// parallel-visualization-of-scientific-data-using-blender
+//Emacs compile interactive
+
+#include<iostream> 
+using namespace std
+
+class A
+{public:
+  int i;
+
+  A(int n=0): i(n).
+  {cout<<"constr A adresse "<<this<<endl; }
+
+  ~A() { cout <<" destr A adresse "<<this<<endl; }
+
+  void Affiche () { cout <<"i="<<i<<endl; }
+};
+
+
+
+class B public A
+{public:
+
+  int j;
+  B(int n=0, int m=0): A(n), j(m)
+    { cout <<"constr B - adresse "<<this<<endl; }
+
+  ~B() { cout <<<" destr B adresse "<<this<<endl; }
+
+  void Affiche () {A::Affiche(); cout<<"j="<<j<<endl; }
+};
+
+
+void main()
+{ A a; a.Affiche();
+  cout<<"---"<<endl;
+  B b; b.Affiche();
+  cout<<"---"<<endl;
+  A * pa;
+  pa=new A(5);pa->Affiche(); delete pa;
+  cout<<"---"<<endl;
+
+  pa=new B(2,5); pa->Affiche(); delete pa;
+  cout<<"---"<<endl;
+}
